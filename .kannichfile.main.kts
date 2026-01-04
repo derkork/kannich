502a9128f610e8944e21299cdbd3d61764afa258
Jan Thomä <janthomae@janthomae.de>
2026-01-04 17:56:41 +0100

    feat: add trivy scan during release

--- .kannichfile.main.kts
+++ .kannichfile.main.kts
@@ -2,55 +2,88 @@
 @file:DependsOn("dev.kannich:kannich-maven:0.1.0")
 @file:DependsOn("dev.kannich:kannich-java:0.1.0")
 @file:DependsOn("dev.kannich:kannich-trivy:0.1.0")
-@file:DependsOn("dev.kannich:kannich-helm:0.1.0")
 
 
 import dev.kannich.java.Java
 import dev.kannich.maven.Maven
 import dev.kannich.stdlib.*
 import dev.kannich.stdlib.tools.*
+import dev.kannich.trivy.Trivy
 
 pipeline {
     val java = Java("21")
     val maven = Maven("3.9.6", java)
+    val trivy = Trivy("0.68.2")
 
-    val deploy = job("deploy") {
+    val release = job("release") {
         // fail fast, verify required variables
-        val dockerUsername = getEnv("KANNICH_DOCKER_USERNAME") ?: fail("Please specify username for docker login in KANNICH_DOCKER_USERNAME")
-        val dockerPassword = getEnv("KANNICH_DOCKER_PASSWORD") ?: fail("Please specify password/token for docker login in KANNICH_DOCKER_PASSWORD")
-        val version = getEnv("KANNICH_VERSION") ?: fail("Please specify release version in KANNICH_VERSION.")
-        val isLatest = getEnv("KANNICH_IS_LATEST") ?: "true"
+        val dockerUsername = getEnv("KANNICH_DOCKER_USERNAME")
+            ?: fail("Please specify username for docker login in KANNICH_DOCKER_USERNAME")
+        val dockerPassword = getEnv("KANNICH_DOCKER_PASSWORD")
+            ?: fail("Please specify password/token for docker login in KANNICH_DOCKER_PASSWORD")
+        val version =
+            getEnv("KANNICH_RELEASE_VERSION") ?: fail("Please specify release version in KANNICH_RELEASE_VERSION.")
+        val setLatest = "true" == (getEnv("KANNICH_SET_LATEST") ?: "true")
+        val dryRun = "true" == (getEnv("KANNICH_DRY_RUN") ?: "false")
 
         // check docker login.
         Docker.login(dockerUsername, dockerPassword)
 
         // set version to desired version
-        maven.exec("-B", "versions:set", "-DnewVersion=$version")
+        log("Setting version to $version")
+        maven.exec("-B", "-q", "versions:set", "-DnewVersion=$version")
 
-        // build jar and image
-        maven.exec("-B", "-Pbootstrap", "install")
+        // build jar and docker image
+        log("Building jar and docker image")
+        maven.exec("-B", "-q", "-Pbootstrap", "install")
+
+        // run trivy on docker image to detect vulnerabilities
+        log("Checking for vulnerabilities in docker image")
+        trivy.exec(
+            "image",
+            "derkork/kannich:$version",
+            "--exit-code", "1",
+            "--exit-on-eol", "1",
+            "--severity", "CRITICAL",
+            "--no-progress",
+            "--format", "json",
+            "--output", "trivy-results.json"
+        )
+
+        artifacts {
+            includes("trivy-results.json")
+        }
+
 
         // push version to docker hub
-        Docker.exec("push", "derkork/kannich:$version")
-
-        if (isLatest == "true") {
-            Docker.exec("tag", "derkork/kannich:$version", "derkork/kannich:latest")
-            Docker.exec("push", "derkork/kannich:latest")
+        if (!dryRun) {
+            log("Publishing docker image to docker hub")
+            Docker.exec("push", "derkork/kannich:$version")
+            // also push to the "latest" tag if desired
+            if (setLatest) {
+                log("Setting latest tag")
+                Docker.exec("tag", "derkork/kannich:$version", "derkork/kannich:latest")
+                Docker.exec("push", "derkork/kannich:latest")
+            }
+        } else {
+            log("Dry run: not pushing to docker hub.")
         }
+
+
     }
 
 
-    execution("deploy") {
-        job(deploy)
+    execution("release") {
+        job(release)
     }
 
 
     execution("test") {
         job("test") {
             secret("World!")
-            logger.info("Getenv: ${getEnv("NOOT")}")
+            log("Getenv: ${getEnv("NOOT")}")
             Shell.execShell("echo 'Hello World!'")
-            logger.info("Hello World!")
+            log("Hello World!")
         }
     }
 }
