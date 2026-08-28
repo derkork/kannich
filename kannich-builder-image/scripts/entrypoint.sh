ed834f29024b7c898853f219a246f2fa678d42ea
Jan Thomä <janthomae@janthomae.de>
2026-08-28 13:47:53 +0200

    fix: properly handle spaces in kannich wrapper arguments

--- kannich-builder-image/scripts/entrypoint.sh
+++ kannich-builder-image/scripts/entrypoint.sh
@@ -4,7 +4,7 @@
 if [ $# -gt 0 ]; then
     # if a command was passed run kannich with
     # the arguments
-    /kannich/jdk/bin/java -jar /kannich/kannich-cli.jar $@
+    /kannich/jdk/bin/java -jar /kannich/kannich-cli.jar "$@"
     exit $?
 fi
 
